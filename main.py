1af51ee07c92d149ebe0b3443569242203a6ddee
04d <81015031+04d@users.noreply.github.com>
2022-04-27 16:39:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def menu_number():
     global random_number
     choice_number_1 = int(input("Veuillez entrer le premier nombre qui sera le nombre de départ : "))
     choice_number_2 = int(input("Veuillez entrer le deuxième nombre qui sera le nombre de d'arrivé : "))
-    if choice_number_2 <= choice_number_1: # vérifie que le second nombre est supérieur au premier.
+    if choice_number_2 <= choice_number_1: 
         os.system('cls||clear')
         print("Vérifiez bien que votre second nombre est supérieur au premier !")
         menu_number()
@@ -30,24 +30,29 @@ def menu_number():
         random_number = random.randint(choice_number_1, choice_number_2)
         number_check()
 
+
 def number_check():
     choice_random = int(input(f"Essayer de trouver le nombre, il est entre {choice_number_1} et {choice_number_2} : "))
-    if choice_random == random_number:
+    if choice_random == random_number: 
         print(f"Bien-Joué vous avez trouvé, c'était bien {random_number} !")
         time.sleep(5)
         replay_game() 
-    elif choice_random != random_number:
+    elif choice_random != random_number: 
         os.system('cls||clear')
         print(f"Ce n'est pas {choice_random}")
-        if choice_random not in not_good_number:
+        if choice_random not in not_good_number: 
             if choice_random >= choice_number_1 and choice_random <= choice_number_2: 
                 not_good_number.append(choice_random) 
-                not_good_number.sort() 
-            elif choice_random <= choice_number_1:
+                not_good_number.sort()
+            if choice_random < random_number:
+                print("C'est plus haut !")
+            elif choice_random > random_number:
+                print("C'est plus bas !")
+            elif choice_random <= choice_number_1: 
                 print(f"{choice_random} est en dessous de {choice_number_1}")
             elif choice_random >= choice_number_2: 
                 print(f"{choice_random} est au dessus de {choice_number_2}")
         print(f"Voici la liste des nombres qui sont faux : {not_good_number} ")
         number_check()
 
-menu_number() 
+menu_number()
